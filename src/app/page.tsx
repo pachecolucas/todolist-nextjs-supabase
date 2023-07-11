a4920671012634290d0f85cf6d4ae7186d9cc704
Lucas Pacheco Teixeira <lucas@eliti.com.br>
2023-07-11 19:11:58 -0300

    exemplo rodando de hoje

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,20 @@
-import Image from "next/image";
+import sql from "./db";
 
-export default function Home() {
+export default async function Home() {
+  const tarefas = await sql`SELECT id, descricao FROM tarefas`;
+
   return (
     <section className="flex justify-center items-center min-h-screen">
       <div className="bg-slate-200 text-gray-900 p-6 border rounded-[50px]">
         <input className="px-6 py-3 rounded-full" />
         <div className="mt-5 uppercase">To do</div>
         <ul>
-          <li className="py-1 flex items-center">
-            <input type="checkbox" className="p-1 w-4 h-4" />
-            <span className="ml-2">Task number 1</span>
-          </li>
-          <li className="py-1 flex items-center">
-            <input type="checkbox" className="p-1 w-4 h-4" />
-            <span className="ml-2">Task number 2</span>
-          </li>
-          <li className="py-1 flex items-center">
-            <input type="checkbox" className="p-1 w-4 h-4" />
-            <span className="ml-2">Task number 3</span>
-          </li>
+          {tarefas.map((t) => (
+            <li key={t.id} className="py-1 flex items-center">
+              <input type="checkbox" className="p-1 w-4 h-4" />
+              <span className="ml-2">{t.descricao}</span>
+            </li>
+          ))}
         </ul>
         <div className="mt-5 uppercase">To do</div>
         <ul>
